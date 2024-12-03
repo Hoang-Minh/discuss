ecb5ee79b1ec9839b59e829609ee49290bd1b9ab
Minh Nguyen <minh.nguyen@ocit.ocgov.com>
2024-12-02 16:28:32 -0800

    Added topic create form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,23 @@
 "use server";
+import { z } from "zod";
 
-export async function createTopic() {
-    // TODO: revalidate the home page
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 }
